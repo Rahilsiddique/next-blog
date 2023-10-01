1337790cd6811580a14efedef33384c787898cb8
Rahilsiddique <siddiquerahil19@gmail.com>
2023-10-01 12:34:27 +0530

    [fix] duplicate author names
    Signed-off-by: Rahilsiddique <siddiquerahil19@gmail.com>

--- src/app/authors/page.tsx
+++ src/app/authors/page.tsx
@@ -3,9 +3,16 @@ import Link from "next/link";
 
 const page = () => {
   const data = getPostMetadata();
+  const newData = data.reduce((prev: any, curr: any) => {
+    if (!prev.some((e: any) => e?.author === curr.author)) {
+      prev.push(curr);
+    }
+    return prev;
+  }, []);
+
   return (
     <div>
-      {data.map((e, i) => (
+      {newData.map((e: any, i: any) => (
         <Link key={i} href={`authors/${e.author}`}>
           <div key={i} className="cursor-pointer">
             {e.author}

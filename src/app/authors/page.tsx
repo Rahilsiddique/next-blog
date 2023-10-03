b9aea9cb63d677252921fd580cb1b9e74f6fed82
amit <sutharamit707@gmail.com>
2023-10-03 16:53:23 +0530

    added author-cards & author-icons at PostPreview section

--- src/app/authors/page.tsx
+++ src/app/authors/page.tsx
@@ -1,23 +1,31 @@
 import { getPostMetadata } from "@/helper/getPostMetadata";
+import { getPostsByAuthor } from "@/helper/getPostsByAuthor";
 import Link from "next/link";
 
 const page = () => {
   const data = getPostMetadata();
   const newData = data.reduce((prev: any, curr: any) => {
+    const posts = getPostsByAuthor(curr.author);
     if (!prev.some((e: any) => e?.author === curr.author)) {
-      prev.push(curr);
+      prev.push({ ...curr, posts });
     }
     return prev;
   }, []);
 
   return (
-    <div>
+    <div className="author-card">
       {newData.map((e: any, i: any) => (
-        <Link key={i} href={`authors/${e.author}`}>
-          <div key={i} className="cursor-pointer">
-            {e.author}
-          </div>
-        </Link>
+        <div className="author-card-content" key={i}>
+          <div className="author-pfp">{e.author.charAt(0).toUpperCase()}</div>
+          <Link href={`authors/${e.author}`} passHref>
+            <div className="cursor-pointer font-semibold hover:underline md:text-xl">
+              {e.author}
+            </div>
+          </Link>
+          <p className="text-xs md:text-sm font-medium">
+            Blogs by author: {e.posts.length}
+          </p>
+        </div>
       ))}
     </div>
   );

e1943d168774c40bb69cc782ecc280662c208134
sirish <sirish1527@gmail.com>
2023-10-26 00:50:01 +0530

    Adding tag management through url #22

--- src/app/tags/page.tsx
+++ src/app/tags/page.tsx
@@ -1,25 +1,41 @@
 import { getPostMetadata } from "@/helper/getPostMetadata";
 import Link from "next/link";
 
-const page = () => {
+interface searchProps {
+  searchParams: { ascDesc?: string };
+}
+export default async function tagPage({ searchParams }: searchProps) {
+  const ascDesc = searchParams.ascDesc;
+  const toggleOptions = ["asc", "desc"];
   const data = getPostMetadata();
   const tags = data.flatMap((e) => e.tags.flat((tag: any) => tag));
   const finalTags = tags.reduce((acc: any, curr: any) => {
     acc[curr] = (acc[curr] || 0) + 1;
     return acc;
   }, {});
-
+  const sortedList = [...Object.entries(finalTags)].sort((a, b) => {
+    if (ascDesc === "asc") return a[0].localeCompare(b[0]);
+    else return b[0].localeCompare(a[0]);
+  });
   return (
     <div className="flex justify-self-center gap-2 cursor-pointer max-w-2xl flex-wrap">
-      {Object.entries(finalTags).map((e, i) => (
-        <Link key={i} href={`tags/${e[0]}`}>
+      <div>
+        <label>Sort Order: </label>
+
+        {toggleOptions.map((option, i) => (
+          <Link href={`?ascDesc=${option}`} key={i}>
+            {option === "asc" ? "Ascending" : "Descending"}
+          </Link>
+        ))}
+      </div>
+
+      {sortedList.map(([tagName, count], i) => (
+        <Link key={i} href={`tags/${tagName}`}>
           <div className="border border-blue-400 px-2 rounded-lg hover:bg-blue-400">
-            {e[0] + " " + e[1]}
+            {tagName + " " + count}
           </div>
         </Link>
       ))}
     </div>
   );
-};
-
-export default page;
+}
